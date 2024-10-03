c669e6686097185acb08f919192070c8b27f8acd
shockfox <shockfox@209-4>
2024-10-03 13:03:29 +0200

    Добавьте файлы проекта.

---
+++ ds/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ МЫ ОПРЕДЕЛИМ в какой строке находится значение с порядковым номером ВВЕДИТЕ n:");
+    int n = int.Parse(Console.ReadLine());
+    int kolonka = 5;
+    int stolb = (n - 1) / kolonka;
+    int rowNumber = stolb + 1;
+    Console.WriteLine("Значение с порядковым номером " + n + " находится в строке " + rowNumber);
+}
+catch
+{
+    Console.WriteLine("введите правильно");
+}
